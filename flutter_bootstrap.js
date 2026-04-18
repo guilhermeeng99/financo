0d50087f141daa5edbfa372494ff30921ea0ad7e
guilhermeeng99 <guilhermeeng99@users.noreply.github.com>
2026-04-18 12:34:13 +0000

    deploy: 85ddf5fa0b5c3d28833c245c89c1ab9569622d5a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorker: {
-    serviceWorkerVersion: "2120976405" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "408288555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
   onEntrypointLoaded: async function(engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
