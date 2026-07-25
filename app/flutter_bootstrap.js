437c3c317ce284b51f76a0c3facaa1086c55dffd
guilhermeeng99 <guilhermeeng99@users.noreply.github.com>
2026-07-25 12:14:11 +0000

    deploy: 7decd65c7aa26d4d99363871656d2dcef5463eed

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -33,12 +33,12 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c0010","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 
 _flutter.loader.load({
   serviceWorker: {
-    serviceWorkerVersion: "2838984107" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "3131789174" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
   onEntrypointLoaded: async function(engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
